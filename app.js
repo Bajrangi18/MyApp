a8d182ac9e72fe4ff9fa4d8f8d025a06310f8083
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-06-23 19:50:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js'
+  import { getDatabase, ref, set  } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-database.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCZyWPmFktra6v3pJnuy5mN_FCUYTsVumE",
+    authDomain: "autopark-f61ef.firebaseapp.com",
+    databaseURL: "https://autopark-f61ef-default-rtdb.firebaseio.com",
+    projectId: "autopark-f61ef",
+    storageBucket: "autopark-f61ef.appspot.com",
+    messagingSenderId: "164346234866",
+    appId: "1:164346234866:web:a8fb9a635091dbfe5c8d6b",
+    measurementId: "G-M0KXQNBN1W"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+
+  const database = getDatabase(app);
