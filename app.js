a8b92855b555642c93041daedb8d328f27e19943
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-06-23 20:29:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js'
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.3/firebase-database.js'
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyCZyWPmFktra6v3pJnuy5mN_FCUYTsVumE",
-    authDomain: "autopark-f61ef.firebaseapp.com",
-    databaseURL: "https://autopark-f61ef-default-rtdb.firebaseio.com",
-    projectId: "autopark-f61ef",
-    storageBucket: "autopark-f61ef.appspot.com",
-    messagingSenderId: "164346234866",
-    appId: "1:164346234866:web:a8fb9a635091dbfe5c8d6b",
-    measurementId: "G-M0KXQNBN1W"
-  };
+    const firebaseConfig = {
+  apiKey: "AIzaSyCZyWPmFktra6v3pJnuy5mN_FCUYTsVumE",
+  authDomain: "autopark-f61ef.firebaseapp.com",
+  databaseURL: "https://autopark-f61ef-default-rtdb.firebaseio.com",
+  projectId: "autopark-f61ef",
+  storageBucket: "autopark-f61ef.appspot.com",
+  messagingSenderId: "164346234866",
+  appId: "1:164346234866:web:a8fb9a635091dbfe5c8d6b",
+  measurementId: "G-M0KXQNBN1W"
+};
 
+  const app = initializeApp(firebaseConfig);
 
-  // const app = initializeApp(firebaseConfig);
-  //
-  // const database = getDatabase(app);
-
-   const app = firebase.initializeApp(firebaseConfig);
-   const db = firebaseApp.database(app);
-   const auth = firebaseApp.auth();
+  const db = const database = getDatabase(app);
